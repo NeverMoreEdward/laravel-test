8915232349112fe826f96f9e6dd448ff49f3cada
NeverMoreEdward <18551397@qq.com>
2025-01-05 13:34:13 +1000

    add test page

---
+++ resources/views/test.blade.php
@@ -0,0 +1,15 @@
+<?php
+// 输出 PHP 信息
+phpinfo();
+
+// 打印 Hello World
+echo "Hello, World!";
+
+// 测试变量
+$test_variable = "This is a test variable!";
+echo "<br>" . $test_variable;
+
+// 测试数组
+$test_array = array(1, 2, 3, 4, 5);
+echo "<br>Array content: " . implode(", ", $test_array);
+?>
